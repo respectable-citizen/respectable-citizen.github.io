0939ddfa556ef73619104700033a3a8f11a67c57
respectable-citizen <77924979+respectable-citizen@users.noreply.github.com>
2023-03-15 16:06:03 +0800

    Update anilist.py

--- .github/scripts/anilist.py
+++ .github/scripts/anilist.py
@@ -16,7 +16,7 @@ def do_paginated_search(json, path):
         result_list = r.json()["data"]
         
         print(r.headers["X-RateLimit-Remaining"], "requests remaining")
-        if r.headers["X-RateLimit-Remaining"] == "1":
+        if r.headers["X-RateLimit-Remaining"] == "5":
             wait_until = first_request_timestamp + 60
             first_request_timestamp = time.time()
             wait_duration = wait_until - first_request_timestamp
